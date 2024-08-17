c66366ad3c4a783e5183bf9159df71898fd059bd
ThiagoFBastos <thiagobastos1705@gmail.com>
2024-08-17 10:35:54 -0300

    Implementando o desafio

--- Program.cs
+++ Program.cs
@@ -1,3 +1,11 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone(numero: "21912345678", modelo: "Pro Max 15", imei: "TESTE", memoria: 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Nokia nokia = new Nokia(numero: "21912345678", modelo: "Tijolão", imei: "TESTE", memoria: 32);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Instagram");
